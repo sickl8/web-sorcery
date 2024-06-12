2ef5a0e69fb6106a89beaa24aa51f56b470be20e
Rich Harris <rich.harris@vercel.com>
2024-06-12 09:08:47 -0400

    unnecessary

--- test/sorcery.test.js
+++ test/sorcery.test.js
@@ -514,8 +514,6 @@ describe('cli', () => {
 								.map((name) => {
 									var contents = fs
 										.readFileSync(`${subdir}/${name}`, 'utf-8')
-										.replace(/\r\n/g, '\n')
-										.replace(/\\r\\n/g, '\\n')
 										.trim();
 
 									if (path.extname(name) === '.map') {
